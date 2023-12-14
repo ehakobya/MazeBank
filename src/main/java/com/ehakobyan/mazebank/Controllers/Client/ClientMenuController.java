764934dde37a2033a661637c543b69dd44cf70d4
Edgar Hakobyan <ehakobya@asu.edu>
2023-12-13 21:03:11 -0800

    Add client menu panel UI

---
+++ src/main/java/com/ehakobyan/mazebank/Controllers/Client/ClientMenuController.java
@@ -0,0 +1,21 @@
+package com.ehakobyan.mazebank.Controllers.Client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button dashboard_button;
+    public Button transaction_button;
+    public Button account_button;
+    public Button profile_button;
+    public Button logout_button;
+    public Button report_button;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
