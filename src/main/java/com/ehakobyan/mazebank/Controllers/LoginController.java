3695cdd4fcd984cf8cb1ecaa3a584d76ef828230
Edgar Hakobyan <ehakobya@asu.edu>
2023-12-17 12:30:20 -0800

    Add client dashboard UI

--- src/main/java/com/ehakobyan/mazebank/Controllers/LoginController.java
+++ src/main/java/com/ehakobyan/mazebank/Controllers/LoginController.java
@@ -1,11 +1,17 @@
 package com.ehakobyan.mazebank.Controllers;
 
+import com.ehakobyan.mazebank.Models.Model;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     public ChoiceBox account_selector;
     public Label payee_address_label;
     public TextField payee_address_input_field;
@@ -13,4 +19,15 @@ public class LoginController {
     public TextField password_input_field;
     public Button login_button;
     public Label error_label;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        login_button.setOnAction(actionEvent -> onLogin());
+    }
+
+    private void onLogin() {
+        Stage stage = (Stage) error_label.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+        Model.getInstance().getViewFactory().showClientWindow();
+    }
 }
--- src/main/java/com/ehakobyan/mazebank/Models/Model.java
+++ src/main/java/com/ehakobyan/mazebank/Models/Model.java
@@ -1,4 +1,23 @@
 package com.ehakobyan.mazebank.Models;
 
+import com.ehakobyan.mazebank.Views.ViewFactory;
+
 public class Model {
+    private static Model model;
+    private final ViewFactory viewFactory;
+
+    private Model() {
+        this.viewFactory = new ViewFactory();
+    }
+
+    public static synchronized Model getInstance() {
+        if (model == null) {
+            model = new Model();
+        }
+        return model;
+    }
+
+    public ViewFactory getViewFactory() {
+        return viewFactory;
+    }
 }
