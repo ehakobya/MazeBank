8556ee1c5b333f520fef9313f5b16f71eb8b512c
Edgar Hakobyan <ehakobya@asu.edu>
2023-12-17 12:41:59 -0800

    Add transactions view panel UI

--- src/main/java/com/ehakobyan/mazebank/Views/ViewFactory.java
+++ src/main/java/com/ehakobyan/mazebank/Views/ViewFactory.java
@@ -7,8 +7,10 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class ViewFactory {
+    
     // Client Views
     private AnchorPane dashboardView;
+    private AnchorPane transactionsView;
 
     public ViewFactory() {}
 
@@ -21,6 +23,17 @@ public class ViewFactory {
             }
         }
         return dashboardView;
+    }
+
+    public AnchorPane getTransactionsView() {
+        if (transactionsView == null) {
+            try {
+                transactionsView = new FXMLLoader(getClass().getResource("/Fxml/Client/Transactions.fxml")).load();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return transactionsView;
     }
 
     public void showLoginWindow() {
